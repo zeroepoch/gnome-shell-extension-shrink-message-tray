a0cb7311b49bde98053f2b8e43592883e6c98a41
Eric Work <work.eric@gmail.com>
2014-11-27 19:57:21 -0800

    Add empty enable and disable functions (required)

--- extension.js
+++ extension.js
@@ -3,3 +3,9 @@ const MessageTraySource = imports.ui.messageTray.Source;
 function init () {
     MessageTraySource.prototype['SOURCE_ICON_SIZE'] = 24;
 }
+
+function enable () {
+}
+
+function disable () {
+}
